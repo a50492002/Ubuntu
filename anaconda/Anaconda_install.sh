691f01d3a0254e0ef2e62a814217d732374f7d89
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-10-14 15:56:59 +0800

    Create Anaconda_install.sh

---
+++ anaconda/Anaconda_install.sh
@@ -0,0 +1,9 @@
+wget https://repo.anaconda.com/archive/Anaconda2-5.2.0-Linux-x86_64.sh
+bash Anaconda2-5.2.0-Linux-x86_64.sh -b
+sudo nano  ~./bashrc
+
+##add by Anaconda3 5.2.0 installer
+#export  PATH="/home/rock/anaconda3/bin:$PATH"
+
+source ~/.bashrc
+python --version
