0ceaa166fa9472bf7e2808ef0a39e198ba0f968a
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-10-14 16:17:19 +0800

    Update Anaconda_install.sh

--- anaconda/Anaconda_install.sh
+++ anaconda/Anaconda_install.sh
@@ -1,5 +1,5 @@
 wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
-bash Anaconda2-5.2.0-Linux-x86_64.sh -b
+bash Anaconda3-5.2.0-Linux-x86_64.sh
 sudo nano  ~./bashrc
 
 ##add by Anaconda3 5.2.0 installer
