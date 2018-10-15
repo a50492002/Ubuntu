a45b4760677111749dd3571f56a41e16d423b50d
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-10-15 14:25:19 +0800

    Update Anaconda_install.sh

--- anaconda/Anaconda_install.sh
+++ anaconda/Anaconda_install.sh
@@ -1,9 +1,19 @@
+# 下載 Anaconda3 5.2.0
 wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+
+# 安裝 Anaconda3 5.2.0
 bash Anaconda3-5.2.0-Linux-x86_64.sh
+
+# 修改環境變數
 sudo nano  ~/.bashrc
+##################################################################
+#    在最後面新增這兩行：                                          #
+#        #add by Anaconda3 5.2.0 installer                       #
+#        export  PATH="/home/<username>/anaconda3/bin:$PATH"     #
+##################################################################
 
-##add by Anaconda3 5.2.0 installer
-#export  PATH="/home/<username>/anaconda3/bin:$PATH"
-
+# 重新編譯環境變數
 source ~/.bashrc
+
+# 查看python版本
 python --version
