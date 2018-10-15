8bb6ba6bb0771637f2c51b0df73a01e651a3d4ba
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-10-15 14:41:53 +0800

    Update remove_anaconda.sh

--- anaconda/remove_anaconda.sh
+++ anaconda/remove_anaconda.sh
@@ -1,7 +1,16 @@
+# 安裝反安裝套件
 conda install anaconda-clean
+
+# 執行反安裝
 anaconda-clean
+
+# 清除殘留檔案
 rm -rf ~/anaconda2
 # or anaconda3
-sudo nano ~/.bashrc
 
-#delete export PATH="/home/<username>/anaconda3/bin:$PATH"
+# 修改環境變數
+sudo nano ~/.bashrc
+###################################################################
+#    刪除這行：                                                    #
+#        export PATH="/home/<username>/anaconda3/bin:$PATH"       #
+###################################################################
