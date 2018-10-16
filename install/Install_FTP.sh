8efe17601bd0e25d03027899c337afafb9a7bb80
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-10-16 16:34:31 +0800

    Create Install_FTP.sh

---
+++ install/Install_FTP.sh
@@ -0,0 +1,23 @@
+sudo apt-get install vsftpd
+
+sudo nano /etc/vsftpd.conf
+#########################################################################################
+#  修改以下參數:                                                                         #
+#      # Allow anonymous FTP? (Disabled by default).                                    #                  
+#     anonymous_enable=NO                                                               #
+#     #                                                                                 #
+#     # Uncomment this to allow local users to log in.                                  #
+#     local_enable=YES                                                                  #
+#       .....                                                                           #
+#     # Uncomment this to enable any form of FTP write command.                         #
+#     write_enable=YES                                                                  #
+#       .....                                                                           #
+#     # You may restrict local users to their home directories.  See the FAQ for        #
+#     # the possible risks in this before using chroot_local_user or                    #
+#     # chroot_list_enable below.                                                       #
+#     chroot_local_user=YES                                                             #
+#     user_sub_token=$USER                                                              #
+#     local_root=/home/$USER/ftp                                                        #
+#########################################################################################
+
+sudo systemctl restart vsftpd
