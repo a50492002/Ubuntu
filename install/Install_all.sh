7d9686e7cb51d11ff969411476e56e84bb8dd061
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-10-18 02:52:23 +0800

    Create Install_all.sh

---
+++ install/Install_all.sh
@@ -0,0 +1,24 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+sudo apt-get install software-properties-common -y
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db 
+sudo add-apt-repository 'deb http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main'
+sudo apt-get update -y
+sudo apt-get install mariadb-server -y
+sudo apt-get install apache2 -y
+sudo apt-get install php php-cgi libapache2-mod-php php-common php-pear php-mbstring -y
+sudo a2enconf php7.0-cgi
+sudo systemctl reload apache2
+sudo apt-get install phpmyadmin php-mbstring php-gettext -y
+sudo apt-get install vsftpd -y
+
+wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+bash Anaconda3-5.2.0-Linux-x86_64.sh
+pip install --upgrade pip
+pip install msgpack
+pip install tensorflow
+pip install keras
+pip install h5py==2.8.0rc1
+jupyter notebook --generate-config
+jupyter notebook password
