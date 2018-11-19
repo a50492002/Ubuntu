17e687b37fb6551cbfa53013b1daa2b0e28605a9
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-11-19 17:48:01 +0800

    Update and rename Install_all.sh to AutoInstall.sh

--- install/AutoInstall.sh
+++ install/AutoInstall.sh
@@ -1,8 +1,13 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 
+#安裝 SSH server
+sudo apt-get install ssh -y
+
+
+#安裝 LAMP
 sudo apt-get install software-properties-common -y
-sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db 
+sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 sudo add-apt-repository 'deb http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main'
 sudo apt-get update -y
 sudo apt-get install mariadb-server -y
@@ -11,8 +16,11 @@ sudo apt-get install php php-cgi libapache2-mod-php php-common php-pear php-mbst
 sudo a2enconf php7.0-cgi
 sudo systemctl reload apache2
 sudo apt-get install phpmyadmin php-mbstring php-gettext -y
+
+#安裝FTP
 sudo apt-get install vsftpd -y
 
+#安裝 Anaconda
 wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
 bash Anaconda3-5.2.0-Linux-x86_64.sh
 pip install --upgrade pip
