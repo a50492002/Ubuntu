5892717a5b7229f377121337581344c83756172e
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-11-22 01:16:38 +0800

    Update Install_ssh.sh

--- install/Install_ssh.sh
+++ install/Install_ssh.sh
@@ -1,4 +1,4 @@
 sudo apt-get install ssh -y
 sudo sed -i 's/Port 22/Port 58888/g' /etc/ssh/sshd_config
-sudo sed -i 's/PermitRootLogin -password/PermitRootLogin no/g' /etc/ssh/sshd_config
+sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
 sudo systemctl restart ssh
