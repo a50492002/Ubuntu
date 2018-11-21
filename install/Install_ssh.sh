4496e0f1a0a3b0975ecb2ded5fddc55b03d171e3
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-11-22 00:58:13 +0800

    Create Install_ssh.sh

---
+++ install/Install_ssh.sh
@@ -0,0 +1,4 @@
+sudo apt-get install ssh -y
+sudo sed -i 's/Port 22/Port 58888/g' /etc/ssh/sshd_config
+sudo sed -i 's/PermitRootLogin without-password/PermitRootLogin no/g' /etc/ssh/sshd_config
+sudo systemctl restart ssh
