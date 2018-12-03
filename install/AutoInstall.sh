d2a75f0199fe67b5b2c009cff70ae3c9ee1db95b
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-12-03 20:41:45 +0800

    Update AutoInstall.sh

--- install/AutoInstall.sh
+++ install/AutoInstall.sh
@@ -2,27 +2,31 @@ sudo apt-get update -y
 sudo apt-get upgrade -y
 
 #安裝 SSH server
-sudo apt-get install ssh -y
+sudo apt-get install -y ssh
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
 sudo sed -i 's/Port 22/Port 58888/g' /etc/ssh/sshd_config
 sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
 sudo systemctl restart ssh
 
 #安裝 LAMP
-sudo apt-get install software-properties-common -y
+sudo apt-get install -y software-properties-common
 sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
 sudo add-apt-repository 'deb http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main'
 sudo apt-get update -y
-sudo apt-get install mariadb-server -y
-sudo apt-get install apache2 -y
-sudo apt-get install php php-cgi libapache2-mod-php php-common php-pear php-mbstring -y
+sudo apt-get install -y mariadb-server
+sudo apt-get install -y apache2
+sudo apt-get install -y php php-cgi libapache2-mod-php php-common php-pear php-mbstring
 sudo a2enconf php7.0-cgi
 sudo systemctl reload apache2
-sudo apt-get install phpmyadmin php-mbstring php-gettext -y
+sudo apt-get install -y phpmyadmin php-mbstring php-gettext
 
 #安裝FTP
-sudo apt-get install vsftpd -y
-
-
+sudo apt-get install -y vsftpd
+sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
+sudo sed -i 's/#write_enadle=YES/write_enadle=YES/g' /etc/ssh/sshd_config
+sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
+sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
+sudo systemctl restart vsftpd
 
 #安裝 Anaconda
 #wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
