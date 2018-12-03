696533fe43d340185c7e1e38c417d166d1566e2b
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-12-03 21:10:35 +0800

    Update AutoInstall.sh

--- install/AutoInstall.sh
+++ install/AutoInstall.sh
@@ -23,7 +23,7 @@ sudo apt-get install -y phpmyadmin php-mbstring php-gettext
 #安裝FTP
 sudo apt-get install -y vsftpd
 sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
-sudo sed -i 's/#write_enadle=YES/write_enadle=YES/g' /etc/ssh/sshd_config
+sudo sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/ssh/sshd_config
 sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
 sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
 sudo chmod 777 /var/www/html
