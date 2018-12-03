2f0bfff91d3b65825d78acbac1344f4e328a1bce
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-12-03 20:42:56 +0800

    Update Install_FTP.sh

--- install/Install_FTP.sh
+++ install/Install_FTP.sh
@@ -1,29 +1,7 @@
-# 安裝FTP
-sudo apt-get install vsftpd
-
-
-# 創建FTP目錄(web用)
-sudo mkdir /mnt/web
-
-# 連結FTP目錄到web目錄
-sudo ln -s /mnt/web/ /var/www/html/
-
-# 編輯設定檔
-sudo nano /etc/vsftpd.conf
-#########################################################################################
-#  修改以下參數:                                                                         #
-#     # Allow anonymous FTP? (Disabled by default).                                     #                  
-#     anonymous_enable=NO                                                               #
-#     #                                                                                 #
-#     # Uncomment this to allow local users to log in.                                  #
-#     local_enable=YES                                                                  #
-#     #                                                                                 #
-#     # Uncomment this to enable any form of FTP write command.                         #
-#     write_enable=YES                                                                  #
-#     .....                                                                             #
-#     local_root=/mnt/web                                                               #
-#                                                                                       #
-#########################################################################################
-
-# 重啟FTP
+sudo apt-get install -y vsftpd
+sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
+sudo sed -i 's/#write_enadle=YES/write_enadle=YES/g' /etc/ssh/sshd_config
+sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
+sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
+sudo chmod 777 /var/www/html
 sudo systemctl restart vsftpd
