7ea324923e76848a7b1b0daf58bbdde502a7296b
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-12-17 10:51:01 +0800

    Update AutoInstall.sh

--- install/AutoInstall.sh
+++ install/AutoInstall.sh
@@ -7,6 +7,7 @@ sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
 sudo sed -i 's/Port 22/Port 58888/g' /etc/ssh/sshd_config
 sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
 sudo systemctl restart ssh
+# sudo ufw allow 58888
 
 #安裝 LAMP
 sudo apt-get install -y software-properties-common
@@ -19,14 +20,31 @@ sudo apt-get install -y php php-cgi libapache2-mod-php php-common php-pear php-m
 sudo a2enconf php7.0-cgi
 sudo systemctl reload apache2
 sudo apt-get install -y phpmyadmin php-mbstring php-gettext
+# sudo ufw allow http
 
 #安裝FTP
 sudo apt-get install -y vsftpd
 sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
-sudo sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/ssh/sshd_config
+sudo sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
 sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
+sudo sed -i '$a force_dot_files=YES' /etc/vsftpd.conf
+sudo sed -i '$a pasv_min_port=50000' /etc/vsftpd.conf
+sudo sed -i '$a pasv_max_port=55000' /etc/vsftpd.conf
 sudo chmod 777 /var/www/html
 sudo systemctl restart vsftpd
+# sudo ufw allow ftp
+# sudo ufw allow 50000:55000/tcp
+
+#防火牆UFW
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 58888
+sudo ufw allow http
+sudo ufw allow https
+sudo ufw allow ftp
+sudo ufw allow ftps
+sudo ufw allow 50000:55000/tcp
+sudo ufw enable
 
 #安裝 Anaconda
 #wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
