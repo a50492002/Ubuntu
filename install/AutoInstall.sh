9586bbd38de4ef5451b09eaa7076410fad6e8be7
a50492002 <43949950+a50492002@users.noreply.github.com>
2018-12-17 09:40:57 +0800

    Update AutoInstall.sh

--- install/AutoInstall.sh
+++ install/AutoInstall.sh
@@ -25,7 +25,6 @@ sudo apt-get install -y vsftpd
 sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
 sudo sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/ssh/sshd_config
 sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
-sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
 sudo chmod 777 /var/www/html
 sudo systemctl restart vsftpd
 
