d8ebf3255dfc8a9c80d6e454ef7c96e15eb7134e
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-01-08 15:07:16 +0800

    Update Install_pip_套件.sh

--- anaconda/Install_pip_套件.sh
+++ anaconda/Install_pip_套件.sh
@@ -1,8 +1,16 @@
+
+pip install --upgrade pip
+
 # 安裝必要元件
 pip install msgpack
 
 # 安裝 tensorflow
 pip install tensorflow
+pip install tf-nightly
+
+# 安裝 支援GPU的 tensorflow
+#pip install tensorflow-gpu
+#pip install tf-nightly-gpu
 
 # 安裝 keras
 pip install keras
@@ -10,7 +18,5 @@ pip install keras
 # 安裝 h5py
 pip install h5py==2.8.0rc1
 
-
-
-sudo apt-get install python-dev
-pip install mysqlclient
+#sudo apt-get install python-dev
+#pip install mysqlclient
