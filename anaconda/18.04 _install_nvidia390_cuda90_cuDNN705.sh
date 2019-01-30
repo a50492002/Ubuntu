c74bcb8058f8c126c88e00df02aa2963f75d2760
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-01-31 05:02:39 +0800

    Create 18.04 _install_nvidia390_cuda90_cuDNN705.sh

---
+++ anaconda/18.04 _install_nvidia390_cuda90_cuDNN705.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:graphics-drivers/ppa
+sudo apt update
+sudo apt install nvidia-390
+
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1704-9-0-local_9.0.176-1_amd64-deb
+sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get install cuda
