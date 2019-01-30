abfba762391ced396f86cbd3215ce43f08a9c208
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-01-31 05:11:41 +0800

    Create Auto_install_18.sh

---
+++ install/Auto_install_18.sh
@@ -0,0 +1,54 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#安裝 SSH server
+sudo apt-get install -y ssh
+sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.backup
+sudo sed -i 's/#Port 22/Port 8797/g' /etc/ssh/sshd_config
+sudo sed -i 's/PermitRootLogin prohibit-password/PermitRootLogin no/g' /etc/ssh/sshd_config
+sudo systemctl restart ssh
+# sudo ufw allow 8797
+
+#安裝 LAMP
+sudo apt-get install -y apache2
+sudo apt-get install -y mariadb-server mariadb-client
+sudo apt-get install -y php php-cgi libapache2-mod-php php-common php-pear php-mbstring php-mysql
+sudo a2enconf php7.2-cgi
+sudo systemctl reload apache2
+sudo apt-get install -y phpmyadmin php-mbstring php-gettext
+# sudo ufw allow http
+
+#安裝FTP
+sudo apt-get install -y vsftpd
+sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
+sudo sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
+sudo sed -i '$a local_root=/var/www/html/' /etc/vsftpd.conf
+sudo sed -i '$a force_dot_files=YES' /etc/vsftpd.conf
+sudo sed -i '$a pasv_min_port=50000' /etc/vsftpd.conf
+sudo sed -i '$a pasv_max_port=55000' /etc/vsftpd.conf
+sudo chmod 777 /var/www/html
+sudo systemctl restart vsftpd
+# sudo ufw allow ftp
+# sudo ufw allow 50000:55000/tcp
+
+#防火牆UFW
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw allow 8797
+sudo ufw allow http
+sudo ufw allow https
+sudo ufw allow ftp
+sudo ufw allow ftps
+sudo ufw allow 50000:55000/tcp
+sudo ufw enable
+
+#安裝 Anaconda
+#wget https://repo.anaconda.com/archive/Anaconda3-5.2.0-Linux-x86_64.sh
+#bash Anaconda3-5.2.0-Linux-x86_64.sh
+#pip install --upgrade pip
+#pip install msgpack
+#pip install tensorflow
+#pip install keras
+#pip install h5py==2.8.0rc1
+#jupyter notebook --generate-config
+#jupyter notebook password
