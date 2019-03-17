289c80a2ca2813f2b88beabffc3ca79856a03e01
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-03-18 07:01:20 +0800

    Update set_mariaDB.sh

--- install/set_mariaDB.sh
+++ install/set_mariaDB.sh
@@ -1,5 +1,5 @@
 sudo mysql -u root
-MariaDB [(none)]> CAREAR USER '<username>'@'%' IDENTIFIED BY '<password>';
+MariaDB [(none)]> CREATE USER '<username>'@'%' IDENTIFIED BY '<password>';
 MariaDB [(none)]> GRANT ALLPRIVILEGES ON *.* TO '<usermame>'@'%' WITH  GRANT OPTION;
 MariaDB [(none)]> exit
 sudo systemctl restart mariadb
