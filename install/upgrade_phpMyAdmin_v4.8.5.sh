0d4a22093a88dab3f3975d59e1f7022d1f50d8d7
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-03-19 23:31:41 +0800

    Update upgrade_phpMyAdmin_v4.8.5.sh

--- install/upgrade_phpMyAdmin_v4.8.5.sh
+++ install/upgrade_phpMyAdmin_v4.8.5.sh
@@ -4,3 +4,12 @@ cd /usr/share/phpmyadmin/
 sudo wget https://files.phpmyadmin.net/phpMyAdmin/4.8.5/phpMyAdmin-4.8.5-all-languages.tar.gz
 sudo tar xzf phpMyAdmin-4.8.5-all-languages.tar.gz
 sudo mv phpMyAdmin-4.8.5-all-languages/* /usr/share/phpmyadmin
+sudo rm /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages.tar.gz
+sudo rm -rf /usr/share/phpmyadmin/phpMyAdmin-4.8.5-all-languages
+sudo rm -rf /usr/share/phpmyadmin.bak
+
+
+
+sudo nano /usr/share/phpmyadmin/libraries/vendor_config.php
+  define('TEMP_DIR', '/var/lib/phpmyadmin/tmp/');
+  define('CONFIG_DIR', '/etc/phpmyadmin/');
