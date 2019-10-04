33280aa8068acc4cb3e48a77ef1431672025779a
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-10-04 10:09:28 +0800

    Update Auto_install_18.sh

--- install/Auto_install_18.sh
+++ install/Auto_install_18.sh
@@ -20,6 +20,8 @@ sudo apt-get install -y phpmyadmin php-mbstring php-gettext
 
 #安裝FTP
 sudo apt-get install -y vsftpd
+sudo mkdir /var/ftp
+sudo chmod 777 /var/ftp
 sudo cp /etc/vsftpd.conf /etc/vsftpd.conf.backup
 sudo sed -i 's/#write_enable=YES/write_enable=YES/g' /etc/vsftpd.conf
 sudo sed -i '$a local_root=/var/ftp/' /etc/vsftpd.conf
@@ -27,8 +29,6 @@ sudo sed -i '$a user_config_dir=/etc/vsftpd/userconfig' /etc/vsftpd.conf
 sudo sed -i '$a force_dot_files=YES' /etc/vsftpd.conf
 sudo sed -i '$a pasv_min_port=50000' /etc/vsftpd.conf
 sudo sed -i '$a pasv_max_port=55000' /etc/vsftpd.conf
-sudo mkdir /var/ftp
-sudo chmod 777 /var/ftp
 sudo mkdir /etc/vsftpd
 sudo mkdir /etc/vsftpd/userconfig
 sudo systemctl restart vsftpd
